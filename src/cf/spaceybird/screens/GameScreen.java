22836835acdeccf4083116c366bb53b9208b5ad7
Bradley Sherman <sirrus233@gmail.com>
2014-02-12 23:58:54 -0800

    GameScreen updated to call Updater.update() and Renderer.render() inside the main loop.

--- src/cf/spaceybird/screens/GameScreen.java
+++ src/cf/spaceybird/screens/GameScreen.java
@@ -1,6 +1,8 @@
 package cf.spaceybird.screens;
 
 import cf.spaceybird.Renderer;
+import cf.spaceybird.Updater;
+
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL10;
@@ -16,6 +18,7 @@ public class GameScreen implements Screen {
 	public void render(float delta) {
 		Gdx.gl.glClearColor(0.1f, 0.1f, 0.1f, 1);
 		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
+		Updater.update();
 		Renderer.render();
 	}
 
