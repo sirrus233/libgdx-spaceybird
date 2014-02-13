25ff929eefc6e49b63c21b978eb688be4bad63b3
Bradley Sherman <sirrus233@gmail.com>
2014-02-12 23:54:44 -0800

    Deprecated SIZE constant in Obstacle class. This info was redundant and is contained in radius of bounding circle. Constructors changed to update bounds of circle to match position vector. Methods to access fields were added.

--- src/cf/spaceybird/Obstacle.java
+++ src/cf/spaceybird/Obstacle.java
@@ -5,24 +5,41 @@ import com.badlogic.gdx.math.Vector2;
 
 public class Obstacle {
 	
-	private static final float SIZE = 1f;
-	private static final float DEFAULT_MASS = 1f;
+	private static final float DEFAULT_MASS = 1;
 	
 	private Vector2 position = new Vector2();
 	private Circle bounds = new Circle();
 	private float mass = DEFAULT_MASS;
 	
-	//Constructs an obstacle at the specified position vector with the specified mass.
-	public Obstacle(Vector2 position, float mass) {
+	//Constructs an obstacle at the specified position vector with the specified mass and radius.
+	public Obstacle(Vector2 position, float radius, float mass) {
 		this.position = position;
-		this.bounds.radius = SIZE;
+		this.bounds.x = position.x;
+		this.bounds.y = position.y;
+		this.bounds.radius = radius;
 		this.mass = mass;
 	}
 	
-	//Constructs an obstacle at the specified position vector with the default mass.
-	public Obstacle(Vector2 position) {
+	//Constructs an obstacle at the specified position vector with specified radius and the
+	//default mass.
+	public Obstacle(Vector2 position, float radius) {
 		this.position = position;
-		this.bounds.radius = SIZE;
+		this.bounds.x = position.x;
+		this.bounds.y = position.y;
+		this.bounds.radius = radius;
 	}
+	
+	public Vector2 getPosition() {
+		return this.position;
+	}
+	
+	public float getMass() {
+		return this.mass;
+	}
+	
+	public float getRadius() {
+		return this.bounds.radius;
+	}
+
 
 }
