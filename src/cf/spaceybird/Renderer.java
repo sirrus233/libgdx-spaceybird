7aea731acc3f67f868960b41c9088fde32ebede9
Bradley Sherman <sirrus233@gmail.com>
2014-02-12 23:57:57 -0800

    Refactored camera code to allow for variable aspect ratio. Each obstacle has a circle rendered for it - but this will be enhanced and largely changed in the future.

--- src/cf/spaceybird/Renderer.java
+++ src/cf/spaceybird/Renderer.java
@@ -7,21 +7,29 @@ import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
 
 public class Renderer {
     
+	public static float aspectX = 16;
+	public static float aspectY = 9;
+	
 	private static OrthographicCamera cam;
 	private static ShapeRenderer debugRenderer = new ShapeRenderer();
 
+	//This method primarily handles the initialization of the Renderer's camera
 	public static void init() {
-    	cam = new OrthographicCamera(10, 7);
-    	cam.position.set(5, 3.5f, 0);
+		//A new OrthographicCamera is created, with relative aspect ratio defined in its parameters
+		//The camera view is then shifted to point at only the first quadrant of the graph
+		cam = new OrthographicCamera(aspectX, aspectY);
+    	cam.position.set(aspectX/2, aspectY/2, 0);
     	cam.update();
     }
 	
 	public static void render() {
         // render blocks
         debugRenderer.setProjectionMatrix(cam.combined);
-        debugRenderer.begin(ShapeType.Line);
+        debugRenderer.begin(ShapeType.Filled);
         debugRenderer.setColor(new Color(1, 0, 0, 1));
-        debugRenderer.rect(1, 2, 3, 5);
+        for (Obstacle o : LevelManager.getObstacles()) {
+        	debugRenderer.circle(o.getPosition().x, o.getPosition().y, o.getRadius(), 1200);
+        }
         debugRenderer.end();
 	}
 }
