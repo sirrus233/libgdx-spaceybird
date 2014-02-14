3a17378ea36090c30ed8f1cfde0f690fd79810c0
Bradley Sherman <sirrus233@gmail.com>
2014-02-14 00:36:04 -0800

    Revised gravitational method in physics engine to rely on position vectors instead of points. Also added getVelocity and getAcceleration methods.

--- src/cf/spaceybird/PhysicsEngine.java
+++ src/cf/spaceybird/PhysicsEngine.java
@@ -1,31 +1,38 @@
 package cf.spaceybird;
 
-import java.awt.geom.Point2D;
 import com.badlogic.gdx.math.Vector2;
 
 /*
 The PhysicsEngine class contains a number of static methods useful for calculating
-various object physics (e.g. gravitational attraction, rising/falling)
+various object physics (e.g. gravitational attraction, kinematics)
 */
 
 public class PhysicsEngine {
     public static final float GRAV_CONST = 1f;
     
-    //Empty PhysicsEngine constructor
-    public PhysicsEngine() {       
-    }
-    
-    //Takes input data on two objects, represented as a location (point) and mass
+    //Takes input data on two objects, represented as a position vector and mass
     //for each, and calculates the gravitational force between them. The force is
     //returned as a vector pointing from the position of the first object to the second.
-    public static Vector2 getGravForce(Point2D.Float fromPos, float fromMass, 
-                                       Point2D.Float toPos, float toMass) {
-        float r = (float) fromPos.distance(toPos);
+    public static Vector2 getGravForce(Vector2 fromPos, float fromMass, 
+                                       Vector2 toPos, float toMass) {
+        float r = fromPos.dst(toPos);
         float magnitude = (GRAV_CONST*fromMass*toMass)/(r*r);
         
-        Vector2 force = new Vector2((toPos.x - fromPos.x),(toPos.y - fromPos.y));
+        Vector2 force = new Vector2(toPos.sub(fromPos));
         force.scl(magnitude);
         
         return force;
+    }
+    
+    public static Vector2 getAcceleration(float mass, Vector2 force) {
+    	return force.div(mass);
+    }
+    
+    public static Vector2 getVelocity(Vector2 initialVel, Vector2 acc, float timestep) {
+    	return initialVel.add(acc.scl(timestep));
+    }
+    
+    public static Vector2 getVelocity(Vector2 initialVel, Vector2 force, float mass, float timestep) {
+    	return getVelocity(initialVel, getAcceleration(mass, force), timestep);
     }
 }
