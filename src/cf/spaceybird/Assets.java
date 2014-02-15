2de89012acdf3de4db532b2440d0d02e0d857b97
Bradley Sherman <sirrus233@gmail.com>
2014-02-15 12:56:58 -0800

    Fixed TextureRegion for the large planet.

--- src/cf/spaceybird/Assets.java
+++ src/cf/spaceybird/Assets.java
@@ -19,13 +19,13 @@ public class Assets {
 	}
 
 	public static void load () {
-		gameTextures = loadTexture("assets/spaceybird.png");
-		background = new TextureRegion(background, 910, 30, 1600, 1200);
-		background = new TextureRegion(spaceyBird, 840, 1290, 640, 350);
-		background = new TextureRegion(planetLarge, 110, 530, 670, 590);
-		background = new TextureRegion(planetMedium, 1540, 1260, 590, 570);
-		background = new TextureRegion(planetSmall, 420, 1300, 320, 310);
-		background = new TextureRegion(satellite, 660, 110, 190, 380);
-		background = new TextureRegion(asteroid, 2220, 1350, 170, 190);
+		gameTextures = loadTexture("spaceybird.png");
+		background = new TextureRegion(gameTextures, 910, 30, 1600, 1200);
+		spaceyBird = new TextureRegion(gameTextures, 840, 1290, 640, 350);
+		planetLarge = new TextureRegion(gameTextures, 5, 490, 758, 666);
+		planetMedium = new TextureRegion(gameTextures, 1540, 1260, 590, 570);
+		planetSmall = new TextureRegion(gameTextures, 420, 1300, 320, 310);
+		satellite = new TextureRegion(gameTextures, 660, 110, 190, 380);
+		asteroid = new TextureRegion(gameTextures, 2220, 1350, 170, 190);
 	}
 }
